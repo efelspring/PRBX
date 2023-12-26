c2f8d68e0d3857e9c9798215f80028006a13a71f
efelspring <efelspring@gmail.com>
2023-12-26 22:09:31 +0000

    Changes to be committed: 	modified:   datasetScript.py 	new file:   output_actions_and_stats.csv 	new file:   shortTestScript.txt

--- datasetScript.py
+++ datasetScript.py
@@ -6,24 +6,22 @@ def extract_actions_and_stats(input_file, output_csv):
         transcript = file.read()
 
     # Define a regular expression to capture player actions and associated stats
-    pattern = r'Brennan: (.+roll.+)'
+    pattern = r'Brennan:([ a-zA-Z,.\d])+[rR]oll([ a-zA-Z,.\d])+\n'
 
-    matches = re.findall(pattern, transcript, re.DOTALL)
-    #print(matches)
-    # Create a list of tuples with (action, stat)
-    data = [(action.strip(), stat.strip()) for action, stat in matches]
+    matches = re.findall(pattern, transcript)
+    print(matches)
 
     # Write the data to a CSV file
     with open(output_csv, 'w', newline='', encoding='utf-8') as csvfile:
-        fieldnames = ['action', 'stat']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-        writer.writeheader()
-        for action, stat in data:
-            writer.writerow({'action': action, 'stat': stat})
+        writer = csv.DictWriter(csvfile, fieldnames="matches")
+            
+        for i in matches:
+            print(i)
+            #writer.writerow(i)
 
 if __name__ == "__main__":
-    input_file = "D20ScriptTest.txt"
+    input_file = "shortTestScript.txt"
     output_csv = "output_actions_and_stats.csv"
     
     extract_actions_and_stats(input_file, output_csv)
+
